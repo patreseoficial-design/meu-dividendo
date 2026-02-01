12da45b326eb194b6ff41abb09a47f7e28175087
patreseoficial-design <patreseoficial@gmail.com>
2026-02-01 14:49:54 -0300

    Criar api/server.js

---
+++ api/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import moedas from './moedas.js';
+import top from './top.js';
+import ativo from './ativo.js';
+
+const app = express();
+app.use(express.static('../')); // serve index.html e scripts
+
+app.get('/api/moedas', moedas);
+app.get('/api/top', top);
+app.get('/api/ativo/:symbol', ativo);
+
+app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
