26127b863cc337a04494aac750bfa40f3d058372
patreseoficial-design <patreseoficial@gmail.com>
2026-02-01 20:11:49 -0300

    Atualizar o server.js

--- api/server.js
+++ api/server.js
@@ -1,14 +1,14 @@
-// server.js
+// api/server.js
 import express from 'express';
-import moedas from './scripts/moedas.js';
-import top from './scripts/top.js';
-import ativo from './scripts/ativo.js'; // ou pesquisa.js se você renomeou
+import moedas from '../scripts/moedas.js';
+import top from '../scripts/top.js';
+import ativo from '../scripts/ativo.js'; // ou pesquisa.js se você renomeou
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000; // Render usa porta dinâmica
 
 // Serve arquivos estáticos (index.html, style.css, script.js, logo.png, etc)
-app.use(express.static('.'));
+app.use(express.static('../'));
 
 // Rotas da API
 app.get('/api/moedas', moedas);
