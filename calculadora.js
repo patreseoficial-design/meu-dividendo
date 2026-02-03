8a96780f2099e4d07dd43ebd6b02b3a9584caee1
patreseoficial-design <patreseoficial@gmail.com>
2026-02-03 09:36:48 -0300

    Atualizar o calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -63,11 +63,13 @@ function calcular() {
   const totalJuros = montante - totalInvestido;
 
   // MOSTRA CAIXA DE RESUMO
-  const resResumo = document.getElementById('resultadoResumo');
-  resResumo.style.display = 'block';
-  document.getElementById('resTotalFinal').innerText = `Valor Total Final: R$ ${montante.toFixed(2)}`;
-  document.getElementById('resTotalInvestido').innerText = `Valor Total Investido: R$ ${totalInvestido.toFixed(2)}`;
-  document.getElementById('resTotalJuros').innerText = `Total em Juros: R$ ${totalJuros.toFixed(2)}`;
+  
+const res = document.getElementById('resultadoResumo');
+res.style.display = 'block';
+
+document.getElementById('resTotalFinal').innerText = `Valor Total Final: R$ ${montante.toFixed(2)}`;
+document.getElementById('resTotalInvestido').innerText = `Valor Total Investido: R$ ${totalInvestido.toFixed(2)}`;
+document.getElementById('resTotalJuros').innerText = `Total em Juros: R$ ${totalJuros.toFixed(2)}`;
 
   // MOSTRA TABELA DETALHADA
   document.getElementById('resultado').style.display = 'block';
