51eec5763d8c9ffdbcdda04034d101da9033ffe0
patreseoficial-design <patreseoficial@gmail.com>
2026-02-09 07:47:36 -0300

    Atualizar o db.js

--- scraper/src/db.js
+++ scraper/src/db.js
@@ -1,1 +1,30 @@
-oi
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database("./dados.db");
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS dividendos (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      ticker TEXT,
+      tipo TEXT,
+      data_base TEXT,
+      data_pagamento TEXT,
+      valor TEXT
+    )
+  `);
+
+  db.run(`
+    CREATE TABLE IF NOT EXISTS patrimonio (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      ticker TEXT,
+      tipo TEXT,
+      patrimonio_liquido TEXT,
+      valor_patrimonial TEXT,
+      pvp TEXT,
+      atualizado_em TEXT
+    )
+  `);
+});
+
+module.exports = db;
