25810b27da0d4fd6b163e0ad2c1703918fb677a6
patreseoficial-design <patreseoficial@gmail.com>
2026-02-09 07:45:25 -0300

    Atualizar o index.js

--- scraper/src/index.js
+++ scraper/src/index.js
@@ -1,1 +1,19 @@
-oi
+const { fiis, acoes } = require("./config/ativos");
+const rasparFII = require("./scrapers/fii");
+const rasparAcao = require("./scrapers/acao");
+
+(async () => {
+  console.log("🔄 Iniciando coleta...\n");
+
+  for (const fii of fiis) {
+    const dados = await rasparFII(fii.ticker, fii.url);
+    console.log("FII:", dados);
+  }
+
+  for (const acao of acoes) {
+    const dados = await rasparAcao(acao.ticker, acao.url);
+    console.log("AÇÃO:", dados);
+  }
+
+  console.log("\n✅ Finalizado");
+})();
