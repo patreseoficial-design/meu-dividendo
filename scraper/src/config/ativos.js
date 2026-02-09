f504b99ca1f06c42370cdc086a32fc570235e12c
patreseoficial-design <patreseoficial@gmail.com>
2026-02-09 07:43:06 -0300

    Atualizar o ativos.js

--- scraper/src/config/ativos.js
+++ scraper/src/config/ativos.js
@@ -1,1 +1,10 @@
-oi
+module.exports = {
+  fiis: [
+    { ticker: "MXRF11", url: "https://statusinvest.com.br/fundos-imobiliarios/mxrf11" },
+    { ticker: "HGLG11", url: "https://statusinvest.com.br/fundos-imobiliarios/hglg11" }
+  ],
+  acoes: [
+    { ticker: "ITUB4", url: "https://statusinvest.com.br/acoes/itub4" },
+    { ticker: "PETR4", url: "https://statusinvest.com.br/acoes/petr4" }
+  ]
+};
